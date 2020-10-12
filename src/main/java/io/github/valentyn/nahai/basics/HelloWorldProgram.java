9480d131d42cb92cac2fcaff6143564261a472d0
ValentynNagay <nagayvalentyn@gmail.com>
2020-10-13 01:53:10 +0300

    feat: create a maven based project

---
+++ src/main/java/io/github/valentyn/nahai/basics/HelloWorldProgram.java
@@ -0,0 +1,14 @@
+package io.github.valentyn.nahai.basics;
+
+/*
+ * Task:
+ * Say "Hello!" to the world and tell a couple words about yourself!
+ */
+public class HelloWorldProgram {
+    public static void main(String[] args) {
+        // Use "System.out.println("your message");" to output your messages.
+        // Don't shy to copy the above piece of code to provide as many messages as you want.
+        
+        /* Your code here */
+    }
+}
---
+++ src/main/java/io/github/valentyn/nahai/basics/YourInitials.java
@@ -0,0 +1,32 @@
+package io.github.valentyn.nahai.basics;
+
+/*
+ * Task:
+ * Display your initials using large letters. Each letter should be made of the corresponding regular letter.
+ * 
+ * For example:
+ * 
+ *   XXX               XXX   LLL                    VVV               VVV
+ *    XXX             XXX    LLL                    VVV               VVV
+ *     XXX           XXX     LLL                    VVV               VVV
+ *      XXX         XXX      LLL                    VVV               VVV
+ *       XXX       XXX       LLL                    VVV               VVV
+ *        XXX     XXX        LLL                    VVV               VVV
+ *         XXX   XXX         LLL                    VVV               VVV
+ *          XXX XXX          LLL                    VVV               VVV
+ *         XXX   XXX         LLL                    VVV               VVV
+ *        XXX     XXX        LLL                    VVV               VVV
+ *       XXX       XXX       LLL                    VVV               VVV
+ *      XXX         XXX      LLL                    VVV               VVV
+ *     XXX           XXX     LLL                       VVV         VVV
+ *    XXX             XXX    LLLLLLLLLLLLLLLLLLLL         VVV   VVV
+ *   XXX               XXX   LLLLLLLLLLLLLLLLLLLL            VVV
+ *  
+ */
+
+public class YourInitials {
+    public static void main(String[] args) {
+        System.out.println("My initials are:");
+        /* Your code here */
+    }
+}
