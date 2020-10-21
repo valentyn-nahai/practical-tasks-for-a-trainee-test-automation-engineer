8f94e01fc478545b9f649c47fae0ee13a06bebe5
ValentynNagay <nagayvalentyn@gmail.com>
2020-10-22 00:41:43 +0300

    feat: add tasks for "conditional operator" package

---
+++ src/main/java/io/github/valentyn/nahai/basics/conditional/operator/MonthNames.java
@@ -0,0 +1,40 @@
+package io.github.valentyn.nahai.basics.conditional.operator;
+
+import java.time.Month;
+
+/**
+ * Task:
+ * There is the "monthName" method that returns the name of a month by its number.
+ * Uncomment the program, and fix the method so that the program compiles and works correctly.
+ * Do not forget about handling an incorrect number!
+ */
+public class MonthNames {
+    /*static String monthName(int numberOfMonth) {
+        if (numberOfMonth == 1) {
+            return Month.of(1).name();
+        } else if (numberOfMonth == 2) {
+            return Month.of(2).name();
+        } else if(numberOfMonth == 3) {
+            //...
+        }
+        //...
+    }
+
+    public static void main(String[] args) {
+        System.out.println("The month number is 1: " + monthName(1));
+        System.out.println("The month number is 2: " + monthName(2));
+        System.out.println("The month number is 3: " + monthName(3));
+        System.out.println("The month number is 4: " + monthName(4));
+        System.out.println("The month number is 5: " + monthName(5));
+        System.out.println("The month number is 6: " + monthName(6));
+        System.out.println("The month number is 7: " + monthName(7));
+        System.out.println("The month number is 8: " + monthName(8));
+        System.out.println("The month number is 9: " + monthName(9));
+        System.out.println("The month number is 10: " + monthName(10));
+        System.out.println("The month number is 11: " + monthName(11));
+        System.out.println("The month number is 12: " + monthName(12));
+        System.out.println("The month number is 13: " + monthName(13));
+        System.out.println("The month number is -1: " + monthName(-1));
+        System.out.println("The month number is 1234234: " + monthName(1234234));
+    }*/
+}
---
+++ src/main/java/io/github/valentyn/nahai/basics/conditional/operator/WeightOnSolarSystemPlanets.java
@@ -0,0 +1,27 @@
+package io.github.valentyn.nahai.basics.conditional.operator;
+
+/*
+ * Task:
+ * Create a program that will ask the user about a solar system planet they want to visit.
+ * And calculate the weight according to the user choose that they would have there.
+ * The weight might be calculated using the following formula: weight = mass * surface gravity.
+ * The "mass", "planet" should be asked from the user using Scanner class.
+ * You MUST use only the different flavors of the "if" operator in this task.
+ */
+public class WeightOnSolarSystemPlanets {
+    public static void main(String[] args) {
+        final double SURFACE_GRAVITY_ON_MERCURY = 0.155;
+        final double SURFACE_GRAVITY_ON_VENUS = 0.8975;
+        final double SURFACE_GRAVITY_ON_EARTH = 1.000;
+        final double SURFACE_GRAVITY_ON_MOON = 0.1660;
+        final double SURFACE_GRAVITY_ON_MARS = 0.3507;
+        final double SURFACE_GRAVITY_ON_JUPITER = 2.5374;
+        final double SURFACE_GRAVITY_ON_SATURN = 1.0677;
+        final double SURFACE_GRAVITY_ON_URANUS = 0.8947;
+        final double SURFACE_GRAVITY_ON_NEPTUNE = 1.1794;
+        final double SURFACE_GRAVITY_ON_PLUTO = 0.0899;
+        
+        // your code here
+    }
+}
+  
