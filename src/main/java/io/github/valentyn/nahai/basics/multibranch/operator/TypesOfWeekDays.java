29f194482e58e57be0859b6533f02f36e82f7148
ValentynNagay <nagayvalentyn@gmail.com>
2020-10-22 00:42:17 +0300

    feat: add tasks for "multibranch operator" package

---
+++ src/main/java/io/github/valentyn/nahai/basics/multibranch/operator/TypesOfWeekDays.java
@@ -0,0 +1,16 @@
+package io.github.valentyn.nahai.basics.multibranch.operator;
+
+/**
+ * Task:
+ * Create a program that output the type of a week day according to its number.
+ * The number of the week day the user should be able to specify in the input.
+ * Use Scanner class to read input stream from the keyboard.
+ * Your solution MUST include the usage of the "switch" operator. 
+ * Try to use "case" branches in the most effective way.
+ * Assume for this task that there are only 2 type: working and weekend days.
+ */
+public class TypesOfWeekDays {
+    public static void main(String[] args) {
+        // your code here
+    }
+}
