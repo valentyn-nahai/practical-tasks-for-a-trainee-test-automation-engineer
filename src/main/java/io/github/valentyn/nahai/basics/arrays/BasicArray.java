e02a92c20cb9d1ecbfa462a6a1b8682d3ed17d03
ValentynNagay <nagayvalentyn@gmail.com>
2020-10-25 19:17:18 +0200

    feat: add a task for "arrays" package

---
+++ src/main/java/io/github/valentyn/nahai/basics/arrays/BasicArray.java
@@ -0,0 +1,15 @@
+package io.github.valentyn.nahai.basics.arrays;
+
+/**
+ * Task:
+ * Create a program that will print a table 100 x 100 size of random numbers.
+ * Use a two dimensions array to store numbers and the "for loop" to output them.
+ * 
+ * Создайте программу, которая будет печатать таблицу 100 x 100 размера произвольный чисел.
+ * Используйте 2-х мерный массив чтобы хранить числа и "for loop" для их вывода.
+ */
+public class BasicArray {
+    public static void main(String[] args) {
+        // ...
+    }
+}
