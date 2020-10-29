45455d85010c3e084f4c64c4701637515b97c0be
ValentynNagay <nagayvalentyn@gmail.com>
2020-10-29 14:20:02 +0200

    feat: add a task for "oop.classes" package

---
+++ src/main/java/io/github/valentyn/nahai/oop/classes/BasicClass.java
@@ -0,0 +1,16 @@
+package io.github.valentyn.nahai.oop.classes;
+
+/**
+ * Task:
+ * Create several classes that represent entities of the real world.
+ * Each class has to contain fields (data described the state of an object), methods (actions needed to operate those data).
+ * Another requirements are to provide at least 3 different constructors, one of them should be "default" one.
+ * Also, create a runner class which will be a container with "main" method needed to run the program, instantiate objects an so on. 
+ *
+ * Создайте несколько классов, которые представляют сущности реального мира.
+ * Каждый класс должен содержать поля (данные описывают состояние объекта), методы (действия, необходимые для работы с этими данными).
+ * Еще одно требование - предоставить как минимум 3 разных конструктора, один из которых должен быть "по умолчанию".
+ * Кроме того, создайте runner класс, который будет контейнером с "main" методом, необходимым для запуска программы, создания экземпляров объектов и так далее.
+ */
+public class BasicClass {
+}
